fc6189900701f6b7a689045d1caa3eca2405bfa7
michaeljklein <michael14486@gmail.com>
2015-12-22 03:02:21 -0500

    compiling!!!!

--- leechanimation/main.cpp
+++ leechanimation/main.cpp
@@ -21,14 +21,15 @@
 #include <iomanip>
 #include </usr/local/include/eigen3/Eigen/Dense>
 #include </usr/local/include/eigen3/unsupported/Eigen/MatrixFunctions>
-#include </usr/local/include/pcl-1.7/pcl/point_cloud.h>
-#include </usr/local/include/pcl-1.7/pcl/point_types.h>
-#include </usr/local/include/pcl-1.7/pcl/io/pcd_io.h>
-#include </usr/local/include/pcl-1.7/pcl/ModelCoefficients.h>
-#include </usr/local/include/pcl-1.7/pcl/filters/project_inliers.h>
-#include </usr/local/include/pcl-1.7/pcl/kdtree/kdtree_flann.h>
-#include </usr/local/include/pcl-1.7/pcl/features/normal_3d.h>
-#include </usr/local/include/pcl-1.7/pcl/surface/gp3.h>
+#include </usr/local/include/pcl-1.8/pcl/point_cloud.h>
+#include </usr/local/include/pcl-1.8/pcl/point_types.h>
+#include </usr/local/include/pcl-1.8/pcl/io/pcd_io.h>
+#include </usr/local/include/pcl-1.8/pcl/ModelCoefficients.h>
+#include </usr/local/include/pcl-1.8/pcl/filters/project_inliers.h>
+#include </usr/local/include/pcl-1.8/pcl/kdtree/kdtree_flann.h>
+#include </usr/local/include/pcl-1.8/pcl/features/normal_3d.h>
+#include </usr/local/include/pcl-1.8/pcl/surface/gp3.h>
+#include </usr/local/include/pcl-1.8/pcl/io/vtk_io.h>
 
 using namespace Eigen;
 using namespace std;
@@ -152,7 +153,63 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr computecloud(double power, Matrix24f lmat, M
     return cloud;
 }
 
+void rendercloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, std::string filename){
+    // Normal estimation*
+    pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> n;
+    pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+    tree->setInputCloud (cloud);
+    n.setInputCloud (cloud);
+    n.setSearchMethod (tree);
+    n.setKSearch (20);
+    n.compute (*normals);
+    //* normals should not contain the point normals + surface curvatures
+    
+    // Concatenate the XYZ and normal fields*
+    pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointNormal>);
+    pcl::concatenateFields (*cloud, *normals, *cloud_with_normals);
+    //* cloud_with_normals = cloud + normals
+    
+    // Create search tree*
+    pcl::search::KdTree<pcl::PointNormal>::Ptr tree2 (new pcl::search::KdTree<pcl::PointNormal>);
+    tree2->setInputCloud (cloud_with_normals);
+    
+    // Initialize objects
+    pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
+    pcl::PolygonMesh triangles;
+    
+    // Set the maximum distance between connected points (maximum edge length)
+    gp3.setSearchRadius (0.025);
+    
+    // Set typical values for the parameters
+    gp3.setMu (2.5);
+    gp3.setMaximumNearestNeighbors (100);
+    gp3.setMaximumSurfaceAngle(M_PI/4); // 45 degrees
+    gp3.setMinimumAngle(M_PI/18); // 10 degrees
+    gp3.setMaximumAngle(2*M_PI/3); // 120 degrees
+    gp3.setNormalConsistency(false);
+    
+    // Get result
+    gp3.setInputCloud (cloud_with_normals);
+    gp3.setSearchMethod (tree2);
+    gp3.reconstruct (triangles);
+    
+    // Additional vertex information
+    std::vector<int> parts = gp3.getPartIDs();
+    std::vector<int> states = gp3.getPointStates();
+    
+    pcl::io::saveVTKFile (filename, triangles);
+}
 
+void doitall(int n, Matrix24f lmat, Matrix24f tmat0){
+    double power = (double) (n / (POWERDIVISOR));
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = computecloud(power, lmat, tmat0);
+
+    std::string nstr = std::to_string(n);
+    std::string filename = "mesh_" + nstr + ".vtk";
+    
+    rendercloud(cloud, filename);
+}
 
 int main()
 {
@@ -220,7 +277,7 @@ int main()
 //    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = makecloud(double power, Matrix24f leech, Matrix24f trans);
     
     
-    
+    doitall(0, leech, trans);
     
     
     return 0;
